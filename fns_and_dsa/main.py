5a17bc86ef874e9499e94e3cd3cde210326a2d91
Alai-ne <alaineakeza12@gmail.com>
2025-11-27 03:06:31 -0800

    save my changes

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -5,7 +5,16 @@ print("Arithmetic Operations")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-result = perform_operation(num1, num2, operation)
+if operation == "add":
+    result = num1 + num2
+elif operation  == "substract":
+    result =  num1 - num2
+elif operation == "multiply":
+    result = num1 * num2
+elif operation == "divide":
+    if num2 == 0:
+        print("Error:can't divide by zero !")
+    result = num1 / num2
+else:
+    print("Error invalid operator")
 print(f"Result: {result}")
-if __name__ == "__main__":
-    main()
