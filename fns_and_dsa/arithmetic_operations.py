f82a3c8abf6cc210d100e4e3b570925d1db9e459
Alai-ne <alaineakeza12@gmail.com>
2025-11-27 03:10:27 -0800

    save my edits

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,14 +2,14 @@ def perfom_operation(num1,num2,operation):
     operation = operation.lower().strip()
 
     if operation == "add":
-        result == num1 + num2
+        result = num1 + num2
     elif operation  == "substract":
-        result ==  num1 - num2
+        result =  num1 - num2
     elif operation == "multiply":
-        result == num1 * num2
+        result = num1 * num2
     elif operation == "divide":
         if num2 == 0:
-            return "Error:can't divide by zero !"
-        result == num1 / num2
+            print( "Error:can't divide by zero !")
+        result = num1 / num2
     else: 
-        return "Error invalid operator"
+        print("Error invalid operator")
