9975811d20ee6afad6cd6ea27db4e02058ce6b52
Alai-ne <alaineakeza12@gmail.com>
2025-12-08 03:15:07 -0800

    save

---
+++ oop/main.py
@@ -0,0 +1,25 @@
+ from book_class import Book
+
+# Create an instance of the Book class
+book1 = Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 1979)
+
+# Use the string representation method
+print(str(book1))
+
+# Use the official representation method
+print(repr(book1))
+
+# Delete the object to trigger the destructor
+del book1from book_class import Book
+
+# Create an instance of the Book class
+book1 = Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 1979)
+
+# Use the string representation method
+print(str(book1))
+
+# Use the official representation method
+print(repr(book1))
+
+# Delete the object to trigger the destructor
+del book1
