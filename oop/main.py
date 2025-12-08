103992a456b197872ae81b3123bdaec80c4e158b
Alai-ne <alaineakeza12@gmail.com>
2025-12-08 03:24:42 -0800

    save

--- oop/main.py
+++ oop/main.py
@@ -23,3 +23,22 @@ print(repr(book1))
 
 # Delete the object to trigger the destructor
 del book1
+
+
+from library_system import Library, EBook, PrintBook
+
+# Create a library instance
+my_library = Library()
+
+# Create book instances
+book1 = EBook("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 1500)
+book2 = PrintBook("Dune", "Frank Herbert", 800)
+book3 = Book("1984", "George Orwell")
+
+# Add books to the library
+my_library.add_book(book1)
+my_library.add_book(book2)
+my_library.add_book(book3)
+
+# List all books in the library
+my_library.list_books()
