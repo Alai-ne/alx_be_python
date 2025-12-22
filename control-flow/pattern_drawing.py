024c9f5ef4b6121e333656f685b7d82c39d6bfe8
Alai-ne <alaineakeza12@gmail.com>
2025-12-22 04:13:37 -0800

    save

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,18 @@
-size  = input("Enter the size of the pattern: ")
-row = 0
-while row < size:
-    for col in range(size):
-        print("*", end="")
-
+def draw_square_pattern(size):
+    row_counter = 0
+    while row_counter < size:
+        for _ in range(size):
+            print("*", end="")
         print()
-        row += 1
+        row_counter += 1
+
+# Prompt the user for input
+try:
+    pattern_size = int(input("Enter the size of the pattern:"))
+    if pattern_size > 0:
+        draw_square_pattern(pattern_size)
+    else:
+        print("Please enter a positive integer.")
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+
